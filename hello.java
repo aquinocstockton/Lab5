167d102580a6aadea665cd80ead93174fe844bc9
Chelsea Aquino <aquinoc2@go.stockton.edu>
2019-02-28 10:02:52 -0500

    Hello uses greeter

--- hello.java
+++ hello.java
@@ -1,5 +1,14 @@
 public class Hello 
 {public static void main (String argv[]) {
-System.out.println("Hello, " + argv[0] + "!");
+
+//Default is "WOrld"
+
+String name = "World";
+if(argv.length!=0) {
+	name = argv[0];
+}
+
+Greeter greeter = new Greeter(name);
+System.out.println(greeter.greet());
 }
 }
